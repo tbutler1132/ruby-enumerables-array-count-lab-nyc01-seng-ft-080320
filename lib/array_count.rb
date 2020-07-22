5f083adc5f124043d26bac3f5f3aa1bb5ed620bb
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-22 16:32:37 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -6,6 +6,6 @@ end
 
 def count_empty_strings(array)
   array.count do |element|
-    element == " "
+    element == ""
   end # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
